020bd270027623818efaabe6d08a9a0aec854463
MartinKal <martin.kalaydzhiev@gmail.com>
2023-04-11 08:37:33 +0300

    added validation and custom exception handling.

--- src/main/java/brandwatch/assessment/shop/controller/OrderController.java
+++ src/main/java/brandwatch/assessment/shop/controller/OrderController.java
@@ -3,6 +3,7 @@ package brandwatch.assessment.shop.controller;
 import brandwatch.assessment.shop.dto.CreateOrderRequest;
 import brandwatch.assessment.shop.model.Order;
 import brandwatch.assessment.shop.service.OrderService;
+import brandwatch.assessment.shop.service.OrderValidationService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,13 +14,16 @@ import org.springframework.web.bind.annotation.*;
 public class OrderController {
 
     private final OrderService orderService;
+    private final OrderValidationService validationService;
 
-    public OrderController(OrderService orderService) {
+    public OrderController(OrderService orderService, OrderValidationService validationService) {
         this.orderService = orderService;
+        this.validationService = validationService;
     }
 
     @PostMapping
     public ResponseEntity<Order> createOrder(@RequestBody CreateOrderRequest createOrderRequest) {
+        validationService.validateOrderRequest(createOrderRequest);
         Order savedOrder = orderService.createOrder(createOrderRequest);
         return new ResponseEntity<>(savedOrder, HttpStatus.ACCEPTED);
     }
