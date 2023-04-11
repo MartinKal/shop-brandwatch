b35f44cb60d0c7b3f532ad119eb1b5872fc3f491
MartinKal <martin.kalaydzhiev@gmail.com>
2023-04-11 09:28:33 +0300

    implemented StoreClient class that communicates with the Store via REST

--- src/main/java/brandwatch/assessment/shop/service/OrderService.java
+++ src/main/java/brandwatch/assessment/shop/service/OrderService.java
@@ -1,5 +1,6 @@
 package brandwatch.assessment.shop.service;
 
+import brandwatch.assessment.shop.client.StoreClient;
 import brandwatch.assessment.shop.dto.CreateOrderRequest;
 import brandwatch.assessment.shop.model.Order;
 import brandwatch.assessment.shop.repository.OrderRepository;
@@ -8,15 +9,37 @@ import org.springframework.stereotype.Service;
 @Service
 public class OrderService {
 
-    private static final String STATUS_PENDING = "pending";
+    public static final String STATUS_PENDING = "pending";
+    public static final String STATUS_COMPLETED = "completed";
 
     private final OrderRepository orderRepository;
+    private final StoreClient storeClient;
 
-    public OrderService(OrderRepository orderRepository) {
+    public OrderService(OrderRepository orderRepository, StoreClient storeClient) {
         this.orderRepository = orderRepository;
+        this.storeClient = storeClient;
     }
 
     public Order createOrder(CreateOrderRequest request) {
+        boolean inStock = storeClient.checkStockAvailability(request.getLineItems());
+        Order saved;
+        if (inStock) {
+            saved = createOrderSuccess(request);
+        } else {
+            saved = createPendingOrder(request);
+        }
+        return orderRepository.save(saved);
+    }
+
+    private Order createOrderSuccess(CreateOrderRequest request) {
+        Order order = Order.of(
+                request.getLineItems(),
+                STATUS_COMPLETED
+        );
+        return orderRepository.save(order);
+    }
+
+    private Order createPendingOrder(CreateOrderRequest request) {
         Order order = Order.of(
                 request.getLineItems(),
                 STATUS_PENDING
