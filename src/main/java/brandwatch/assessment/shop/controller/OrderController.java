fbcea7c357bcd383b7017d2b3feb9b1b923b8310
MartinKal <martin.kalaydzhiev@gmail.com>
2023-04-13 09:49:01 +0300

    improvements to order flow

--- src/main/java/brandwatch/assessment/shop/controller/OrderController.java
+++ src/main/java/brandwatch/assessment/shop/controller/OrderController.java
@@ -14,20 +14,16 @@ public class OrderController {
 
     private final OrderService orderService;
     private final OrderValidationService validationService;
-    private final RedisTemplate<String, String> redisTemplate;
 
     public OrderController(OrderService orderService, OrderValidationService validationService, RedisTemplate<String, String> redisTemplate) {
         this.orderService = orderService;
         this.validationService = validationService;
-        this.redisTemplate = redisTemplate;
     }
 
     @PostMapping
     public ResponseEntity<CreateOrderResult> createOrder(@RequestBody CreateOrderRequest createOrderRequest) {
         validationService.validateOrderRequest(createOrderRequest);
-        CreateOrderResult order = orderService.createOrder(createOrderRequest);
-        redisTemplate.opsForValue().set("key3", "pas");
-        return ResponseEntity.ok(order);
+        CreateOrderResult createOrderResult = orderService.createOrder(createOrderRequest);
+        return ResponseEntity.ok(createOrderResult);
     }
-
 }
